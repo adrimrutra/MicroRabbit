f5001ec85c7fd1691c46df26e612278b05ebadc9
Artur Mirda <adrimrutra@gmail.com>
2020-05-15 20:50:01 +0300

    Added MicroRabbit.MVC project

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace MicroRabbit.Banking.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BankingController : ControllerBase
     {
         private readonly ILogger<BankingController> _logger;
